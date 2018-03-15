b43ed90d3776c22e1f9ff858485eb964743fa987
huangke <huangkwell@163.com>
2018-03-16 02:04:53 +0800

    添加migrations

--- typeidea/comment/models.py
+++ typeidea/comment/models.py
@@ -12,11 +12,10 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, verbose_name='文章')
     content = models.CharField(max_length=2000, verbose_name='内容')
     nickname = models.CharField(max_length=50, verbose_name="昵称")
-    website = models.CharField(verbose_name="昵称")
+    website = models.URLField(verbose_name="昵称")
     email = models.EmailField(verbose_name="邮箱")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
     
     class Meta:
         verbose_name = verbose_name_plural = "评论"
         ordering = ['-created_time']
-        
