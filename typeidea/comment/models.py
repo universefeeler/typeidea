e2b033a01e178f20c96b3e1630b5f00752509df6
huangke <huangkwell@163.com>
2018-03-16 01:55:28 +0800

    add comment model

---
+++ typeidea/comment/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from blog.models import Post
+
+
+# Create your models here.
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, verbose_name='文章')
+    content = models.CharField(max_length=2000, verbose_name='内容')
+    nickname = models.CharField(max_length=50, verbose_name="昵称")
+    website = models.CharField(verbose_name="昵称")
+    email = models.EmailField(verbose_name="邮箱")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    
+    class Meta:
+        verbose_name = verbose_name_plural = "评论"
+        ordering = ['-created_time']
+        
